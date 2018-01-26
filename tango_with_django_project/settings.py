d8bb19ad79c9dae4348cee0607a44ba617e0e96a
Han <2288527l@student.gla.ac.uk>
2018-01-26 17:31:16 +0000

    End of 4.2

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -20,6 +20,7 @@ print(os.path.dirname(os.path.dirname(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [STATIC_DIR, ]
