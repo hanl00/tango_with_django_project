eaf788bd634ddef3bb895ed36bc9af040466f90e
Han <2288527l@student.gla.ac.uk>
2018-01-31 16:59:37 +0000

    '5.6'

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'rango',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,7 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+
 
 STATICFILES_DIRS = [STATIC_DIR, ]
 
